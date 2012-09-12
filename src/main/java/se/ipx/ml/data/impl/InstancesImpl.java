d4f07803a2feef1cf3a2bc240de6099086de90cc
fekelund <fredrik@ipx.se>
2012-09-12 23:12:59 +0200

    License infomation

--- src/main/java/se/ipx/ml/data/impl/InstancesImpl.java
+++ src/main/java/se/ipx/ml/data/impl/InstancesImpl.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (C) 2012 Fredrik Ekelund <fredrik@ipx.se>
+ *
+ * This file is part of Decision Trees.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package se.ipx.ml.data.impl;
 
 import java.util.ArrayList;
@@ -42,7 +59,7 @@ public class InstancesImpl<T> implements Instances<T> {
 
 	@Override
 	public Matrix<T> getFeatureMatrix() {
-		return new FeatureMatrix<>(features, 0, numRows, numCols);
+		return new FeatureMatrix<T>(features, 0, numRows, numCols);
 	}
 
 	@Override
@@ -335,7 +352,7 @@ public class InstancesImpl<T> implements Instances<T> {
 			numCols = -1;
 		}
 
-		public Builder<T> addInstance(final T targetValue, @SuppressWarnings("unchecked") final T... featureVector) {
+		public Builder<T> addInstance(final T targetValue, final T... featureVector) {
 			if (targetValue == null || featureVector == null) {
 				throw new NullPointerException();
 			}
